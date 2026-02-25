8454db68125d5144c0f40b55b99b029f5df8616d
Christian Liljedahl <christian@liljedahl.dk>
2026-02-25 13:44:08 +0100

    next

--- Onsdag/Scratchpad1/sketch.js
+++ Onsdag/Scratchpad1/sketch.js
@@ -14,18 +14,33 @@ class Stone {
 let stone1;
 function setup() {
   createCanvas(400, 400);
-  stone1 = new Stone(100,200,0,0,40,0);
+  stone1 = new Stone(100,200,1,1,40,0);
   console.log(stone1);
 }
 
 function draw() {
   background(220);
+// Update the model
+updateStone(stone1);
 
 // Draw the stone from our parametrs of stone1
   drawStone(stone1.x, stone1.y, stone1.r, stone1.size );
 
 }
 
+// Regler - Update the model
+function updateStone(s){
+  // s is a stone object
+  // Update the position with the velocities
+  s.x = s.x + s.vx;
+  s.y = s.y + s.vy;
+  // ?? Noget med friktion... 
+  // Check for wall collisions
+  wallBounce(s,0,width,0,height);
+  // ??? Husk farver
+}
+
+// View
 function drawStone(x,y,r,size){
  // Kan vi tegne noget som ligner en sten?
   push();
@@ -39,4 +54,33 @@ function drawStone(x,y,r,size){
     size*0.2,size*0.6,
     size*0.1,size*0.1,size*0.1,size*0.1);
   pop();
+}
+
+function wallBounce(s, left, right, top, bottom) {
+
+  let radius = s.size * 0.5;
+
+  // Left wall
+  if (s.x - radius < left) {
+    s.x = left + radius;
+    s.vx = Math.abs(s.vx);       // sørg for at bevæge sig mod højre
+  }
+
+  // Right wall
+  if (s.x + radius > right) {
+    s.x = right - radius;
+    s.vx = -Math.abs(s.vx);      // sørg for at bevæge sig mod venstre
+  }
+
+  // Top wall
+  if (s.y - radius < top) {
+    s.y = top + radius;
+    s.vy = Math.abs(s.vy);       // bevæg nedad
+  }
+
+  // Bottom wall
+  if (s.y + radius > bottom) {
+    s.y = bottom - radius;
+    s.vy = -Math.abs(s.vy);      // bevæg opad
+  }
 }
